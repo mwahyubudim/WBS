d89f8d1fc4bcd0d34896437d5f11f72cce83986e
mwahyubudim <m.wahyu.budi.m@symmetrix.co.jp>
2024-06-07 16:57:22 +0900

    init spring

---
+++ src/test/java/team/one/WbsApplicationTests.java
@@ -0,0 +1,13 @@
+package team.one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
